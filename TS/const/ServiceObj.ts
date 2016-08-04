17247f526ec1902f5c4f4ae9b46eab3b9dfefe80
lifu <li.fu@cfischina.com>
2016-08-04 16:07:50 +0800

    提交UI_4

---
+++ TS/const/ServiceObj.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Oliver on 2016-08-04 0004.
+ */
+export class ServiceObj {
+    public CommandHandlerExecutor:string = "CommandHandlerExecutor";
+    public ResourceDocumentTable:string = "ResourceDocumentTable";
+    public DefaultExpressionEngine:string = "DefaultExpressionEngine";
+    public ProcessInstanceFactory:string = "ProcessInstanceFactory";
+
+}
